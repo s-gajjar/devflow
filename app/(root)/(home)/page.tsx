a60d8229f7d547144aebb26de69bc88568c7a467
Shreyansh Gajjar <shreyanshgajjar2005@gmail.com>
2024-07-23 10:19:57 +0530

    add assets and layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,9 +2,11 @@ import {UserButton} from "@clerk/nextjs";
 
 const Home = ()=> {
   return (
-    <div>
-        <UserButton/>
-    </div>
+      <div>
+          <UserButton/>
+          <h1 className="h1-bold">DevFlow</h1>
+
+      </div>
   );
 }
 
