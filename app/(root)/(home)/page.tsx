1b97fafe1865e0315486dac102c3075a89db79e2
Shreyansh Gajjar <shreyanshgajjar2005@gmail.com>
2024-07-23 11:03:12 +0530

    implement theme change

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,11 @@
 import {UserButton} from "@clerk/nextjs";
 
 const Home = ()=> {
+
   return (
       <div>
           <UserButton/>
           <h1 className="h1-bold">DevFlow</h1>
-
       </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Inter, Space_Grotesk} from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
+import {ThemeProvider} from "@/context/ThemeProvider";
 
 
 const inter = Inter({
@@ -31,14 +32,18 @@ export default function RootLayout({
     return (
         <ClerkProvider
             appearance={{
-                elements:{
+                elements: {
                     formButtonPrimary: 'primary-gradient',
                     footerActionLink: 'primary-text-gradient hover:text-primary-500',
                 }
             }}>
-            <html lang="en">
-            <body className={`${inter.className} ${spaceGrotesk.className}`}>{children}</body>
-            </html>
+            <ThemeProvider>
+                <html lang="en">
+                <body className={`${inter.className} ${spaceGrotesk.className}`}>
+                {children}
+                </body>
+                </html>
+            </ThemeProvider>
         </ClerkProvider>
     );
 }
