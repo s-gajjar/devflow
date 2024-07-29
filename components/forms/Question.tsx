1c06fc5c58d7f7285fc580e8826d5a87d9b2b16e
Shreyansh Gajjar <shreyanshgajjar2005@gmail.com>
2024-07-29 10:25:59 +0530

    Question Page UI

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import React, {useRef, useState} from 'react';
+import {Editor} from '@tinymce/tinymce-react';
 import {zodResolver} from "@hookform/resolvers/zod"
 import {useForm} from "react-hook-form"
 import {z} from "zod"
@@ -14,13 +16,18 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import {Input} from "@/components/ui/input"
-import formSchema from "@/lib/validations";
+import {QuestionSchema} from "@/lib/validations";
+import {Badge} from "@/components/ui/badge";
+import Image from "next/image";
+
+const type: any= 'create'
 
 const Question = () => {
+    const editorRef = useRef(null);
+    const [isSubmitting, setisSubmitting] = useState(false);
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof QuestionSchema>>({
+        resolver: zodResolver(QuestionSchema),
         defaultValues: {
             title: "",
             explanation: "",
@@ -28,11 +35,47 @@ const Question = () => {
         },
     })
 
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    function onSubmit(values: z.infer<typeof QuestionSchema>) {
+        setisSubmitting(true);
+        setTimeout(() => {
+            setisSubmitting(false);
+        }, 2000);
+
+        try{
+            //make a async call o your api -> create question
+
+            //navigate to home page
+        }catch(e){
+            console.log(e)
+        }finally{
+            setisSubmitting(false);
+        }
+
+    }
+
+    const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: any) => {
+        if (e.key === 'Enter' && field.name === 'tags') {
+            e.preventDefault();
+            const tagInput = e.target as HTMLInputElement;
+            const tagValue = tagInput.value.trim();
+
+            if (tagValue !== '') {
+                if (tagValue.length > 15) {
+                    return form.setError('tags', {type: 'required', message: 'Tags must be less than 15 characters'});
+                }
+
+                // Ensure field.value is always an array
+                const currentTags = Array.isArray(field.value) ? field.value : [];
+
+                if (!currentTags.includes(tagValue)) {
+                    form.setValue('tags', [...currentTags, tagValue]);
+                    tagInput.value = '';
+                    form.clearErrors('tags');
+                } else {
+                    form.trigger();
+                }
+            }
+        }
     }
 
     return (
@@ -40,7 +83,7 @@ const Question = () => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-10">
                 <FormField
                     control={form.control}
-                    name="explanation"
+                    name="title"
                     render={({field}) => (
                         <FormItem className="flex w-full flex-col">
                             <FormLabel className="paragraph-semibold text-dark400_light800">Question Title <span
@@ -59,13 +102,34 @@ const Question = () => {
                 />
                 <FormField
                     control={form.control}
-                    name="title"
+                    name="explanation"
                     render={({field}) => (
                         <FormItem className="flex w-full flex-col  gap-3">
                             <FormLabel className="paragraph-semibold text-dark400_light800">Detailed explanation of your
                                 question <span className="text-primary-500">*</span></FormLabel>
                             <FormControl className="mt-3.5">
-                                {/*   Add an Editor component here */}
+                                <Editor
+                                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                                    onInit={(_evt, editor) => {
+                                        //@ts-ignore
+                                        editorRef.current = editor
+                                    }}
+                                    initialValue=""
+                                    init={{
+                                        height: 500,
+                                        menubar: false,
+                                        plugins: [
+                                            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                                            'anchor', 'searchreplace codesample', 'visualblocks', 'fullscreen',
+                                            'insertdatetime', 'media', 'table', 'codesample',
+                                        ],
+                                        toolbar: 'undo redo | blocks | ' +
+                                            'bold italic forecolor | alignleft aligncenter ' +
+                                            'alignright alignjustify | bullist numlist |' +
+                                            'codesample ',
+                                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+                                    }}
+                                />
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
                                 Introduce the problem and expand on what you put in the title. Minimum 20 characters.
@@ -82,19 +146,51 @@ const Question = () => {
                             <FormLabel className="paragraph-semibold text-dark400_light800">Tags <span
                                 className="text-primary-500">*</span></FormLabel>
                             <FormControl className="mt-3.5">
-                                <Input
-                                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[54px] border"
-                                    placeholder="Add tags..."
-                                    {...field} />
+                                <>
+                                    <Input
+                                        className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[54px] border"
+                                        placeholder="Add tags..."
+                                        onKeyDown={(e) => handleInputKeyDown(e, field)}
+                                    />
+                                    {Array.isArray(field.value) && field.value.length > 0 && (
+                                        <div className="flex flex-wrap gap-2 mt-2.5">
+                                            {field.value.map((tag: string) => (
+                                                <Badge key={tag} className="flex items-center justify-center gap-2 px-4 py-2 text-sm font-medium capitalize
+                                                bg-gray-200 text-gray-800 rounded-md dark:bg-gray-700 dark:text-gray-200">
+                                                    {tag}
+                                                    <Image
+                                                        src="/assets/icons/close.svg"
+                                                        alt="Close icon"
+                                                        width={12}
+                                                        height={12}
+                                                        className="cursor-pointer object-contain"
+                                                        onClick={() => {
+                                                            const newTags = field.value.filter((t: string) => t !== tag);
+                                                            form.setValue('tags', newTags);
+                                                        }}
+                                                    />
+                                                </Badge>
+                                            ))}
+                                        </div>
+                                    )}
+                                </>
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
-                                Add upto 3 tags to categorize your question.
+                                Add up to 3 tags to categorize your question.
                             </FormDescription>
                             <FormMessage className="text-red-500"/>
                         </FormItem>
                     )}
                 />
-                <Button type="submit">Submit</Button>
+                <Button type="submit" className="primary-gradient w-fit !text-light-900" disabled={isSubmitting}>
+                    {isSubmitting ? (
+                        <>
+                            {type === 'edit' ? 'Editing...' : 'Posting...'}
+                        </>
+                    ):(
+                        type === 'edit' ? 'Edit Question' : 'Ask a Question'
+                        )}
+                </Button>
             </form>
         </Form>
     )
--- lib/validations.ts
+++ lib/validations.ts
@@ -1,13 +1,7 @@
 import {z} from "zod";
 
-const formSchema = z.object({
-    title: z.string().min(5, "Title must be at least 5 characters.").max(130),
-    explanation: z.string().min(100, "Explanation must be at least 5 characters."),
+export const QuestionSchema = z.object({
+    title: z.string().min(5).max(130),
+    explanation: z.string().min(20),
     tags: z.array(z.string().min(1).max(15)).min(1).max(3)
-
-
-
-
 })
-
-export default formSchema;
